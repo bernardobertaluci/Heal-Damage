b9e342eb6806ff55f63f46f9ddb29fcd6fa7a6a7
Danil Alexeev <bernardobertalucii@gmail.com>
2021-12-28 16:23:32 +0700

    correct7

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -20,24 +20,20 @@ public class Health : MonoBehaviour
     }
     public void TakeDamage()
     {
-        if (_health < _healthBar.maxValue)
-        {             
-            if(_health + _damageValue < _healthBar.maxValue)
-                _health += _damageValue;
-            else
-                _health = _healthBar.maxValue;
+        if (_health > _healthBar.minValue)
+        {
+            _health = Mathf.Max(_health - _damageValue, _healthBar.minValue);
+
             HealthChanged?.Invoke(_health);
         }
     }
 
     public void TakeHeal()
     {
-        if(_health > _healthBar.minValue)
+        if(_health < _healthBar.maxValue)
         {
-            if (_health - _healValue > _healthBar.minValue)
-                _health -= _healValue;
-            else
-                _health = _healthBar.minValue;
+            _health = Mathf.Min(_health + _healValue, _healthBar.maxValue);
+
             HealthChanged?.Invoke(_health);
         }  
     }
